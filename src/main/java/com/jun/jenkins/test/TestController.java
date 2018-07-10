60ff483f42d31e9c0e188b73f34af9e1815c3235
Lizijun <lizijun@fanruan.com>
2018-07-10 18:53:40 +0800

    first commit

---
+++ src/main/java/com/jun/jenkins/test/TestController.java
@@ -0,0 +1,16 @@
+package com.jun.jenkins.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/test")
+    @ResponseBody
+    public Object test () {
+        return "Hello Jenkins";
+    }
+}
